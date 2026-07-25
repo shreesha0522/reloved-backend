e51e64f22203428edeea221ee32aa36d92ba7927
Shreesha Shrestha <shreeshashrestha2004@gmail.com>
2026-07-25 20:06:51 +0545

    Include passwordExpired flag in MFA login response

--- controllers/mfaController.js
+++ controllers/mfaController.js
@@ -136,7 +136,13 @@ exports.verifyLoginMFA = async (req, res) => {
     res.status(200).json({
       success: true,
       message: "Login successful",
-      data: { _id: user._id, username: user.username, email: user.email, role: user.role },
+      data: {
+        _id: user._id,
+        username: user.username,
+        email: user.email,
+        role: user.role,
+        passwordExpired: user.isPasswordExpired(),
+      },
     });
   } catch (error) {
     console.error("verifyLoginMFA error:", error.message);
