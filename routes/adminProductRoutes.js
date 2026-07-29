c3451097baafa5a1b8c2133f686af39d82990e9f
Shreesha Shrestha <shreeshashrestha2004@gmail.com>
2026-07-30 04:20:16 +0545

    feat: enforce admin-only access on admin routes

--- routes/adminProductRoutes.js
+++ routes/adminProductRoutes.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const router = express.Router();
-const protect = require("../middleware/authMiddleware");
-const adminOnly = require("../middleware/adminOnly");
+const { protect, adminOnly } = require("../middleware/authMiddleware");
+
 const {
   getPendingProducts,
   approveProduct,
